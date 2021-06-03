d2f9b5a7938c7171d7492f625c94030c1f15e300
diego.carvajal.cardenas@gmail.com <dcarvajalc@psl.com.co>
2021-06-03 15:10:54 -0500

    Add first assured test

--- src/test/java/apitest/demo/DemoApplicationTests.java
+++ src/test/java/apitest/demo/DemoApplicationTests.java
@@ -1,13 +1,53 @@
 package apitest.demo;
 
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
-class DemoApplicationTests {
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static io.restassured.RestAssured.when;
+
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class DemoApplicationTests {
 
 	@Test
 	public void testEquals() {
 		assertEquals(2, 2);
 	}
 
+	@BeforeAll
+	public void setup() {
+
+		RestAssured.
+				filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+
+		RestAssured.baseURI = "https://httpbin.org/";
+	}
+
+	@Test()
+	@DisplayName("verify status code")
+	public void testGetStatusCode() {
+		when().
+				get("/ip").
+				then().
+				statusCode(200);
+	}
+
+	@Test()
+	@DisplayName("verify origin value")
+	public void testGetOriginInBody() {
+		when().
+				get("/ip").
+				then().
+				statusCode(200).
+				body("origin", notNullValue());
+	}
 }
